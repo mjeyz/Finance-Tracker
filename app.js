4dd6937f7b6047c19662e2b4526b2900100b0825
Mudasir Jamshaid <thisismjeyz@gmail.com>
2026-05-23 14:58:27 +0500

    setup for node app
    i have created directory and files needed for express backend and i have created roots for different pages

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("Dashboard.ejs");
+});
+
+app.get("/register", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+});
+
+app.post("/register", (req, res) => {
+});
+
+app.post("/login", (req, res) => {
+
+});
+
+app.listen(port, () => {
+    console.log(`Express Server is Listening on ${port}`);
+});
